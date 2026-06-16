7fb45e22db8f6483205a46b260a8389165647c5d
Darius Jankauskas <kirviz@gmail.com>
2026-06-16 11:19:05 +0100

    Starting UI for the list of tickable habits

--- Daily Habit Tracker/ContentView.swift
+++ Daily Habit Tracker/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+struct Habit: Identifiable {
+    let id = UUID()
+    let name: String
+    var isCompleted = false
+}
+
+private enum AppStyle {
+    static let habitName = Color(.label)
+    static let completedCheckbox = Color.green
+    static let incompleteCheckbox = Color(.tertiaryLabel)
+}
+
 struct ContentView: View {
+    @State private var habits = [
+        Habit(name: "Walk"),
+        Habit(name: "Meditation"),
+        Habit(name: "Gym")
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List($habits) { $habit in
+                Button {
+                    habit.isCompleted.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: habit.isCompleted ? "checkmark.square.fill" : "square")
+                            .foregroundStyle(habit.isCompleted ? AppStyle.completedCheckbox : AppStyle.incompleteCheckbox)
+
+                        Text(habit.name)
+                            .foregroundStyle(AppStyle.habitName)
+                    }
+                }
+                .buttonStyle(.plain)
+            }
+            .navigationTitle("Daily Habits")
         }
-        .padding()
     }
 }
 

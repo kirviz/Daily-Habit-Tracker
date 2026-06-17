8f220ac40fde1b0b752472045c4b0fdc5acbb253
Darius Jankauskas <kirviz@gmail.com>
2026-06-17 08:55:03 +0100

    Made testing plans and set UnitTests to default

--- DailyHabitTrackerUITests/DailyHabitTrackerUITests.swift
+++ DailyHabitTrackerUITests/DailyHabitTrackerUITests.swift
@@ -10,34 +10,28 @@ import XCTest
 final class DailyHabitTrackerUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAddHabitAndMarkComplete() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
-    }
+        app.buttons["Add"].tap()
 
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+        let habitNameField = app.textFields["Habit name"]
+        XCTAssertTrue(habitNameField.waitForExistence(timeout: 2))
+        habitNameField.tap()
+        habitNameField.typeText("Read")
+
+        app.buttons["Save"].tap()
+
+        let readHabit = app.buttons["Read"]
+        XCTAssertTrue(readHabit.waitForExistence(timeout: 2))
+        readHabit.tap()
     }
 }
